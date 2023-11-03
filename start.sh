6e64f16eb06fb836d8706875f723c6b7b61c6164
채희승(Hee seung Chae)(chs4413) <chs4413@ncits.co.kr>
2023-11-03 14:24:59 +0900

    flyio swap memory

--- start.sh
+++ start.sh
@@ -5,7 +5,13 @@
 # run, which is why this file exists in the first place.
 # Learn more: https://community.fly.io/t/sqlite-not-getting-setup-properly/4386
 
-set -ex
+# allocate swap space
+fallocate -l 512M /swapfile
+chmod 0600 /swapfile
+mkswap /swapfile
+echo 10 > /proc/sys/vm/swappiness
+swapon /swapfile
+echo 1 > /proc/sys/vm/overcommit_memory
 
 # Finally start the app
 npx prisma migrate deploy
